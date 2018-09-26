991dfa80ad8534ec64ded1ee48f95749c7aa2e59
Larry Wright <larrywright@gmail.com>
2018-09-25 21:59:19 -0500

    No longer need the deploy script, using CodeBuild to deploy

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BUCKET_NAME=larrywright.me
-set -e
-
-cd site/
-hugo
-aws s3 sync --profile blog-deploy --acl "public-read" --sse "AES256" --delete public/ s3://$BUCKET_NAME --exclude .DS_Store
