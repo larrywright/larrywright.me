7cbc76efa2a2f8bc656d2cb49b1b9b43ca16231f
Larry Wright <larrywright@gmail.com>
2023-08-15 22:23:24 -0500

    Get rid of a couple of files I didn't mean to commit

--- content/photo/best-of-2022/generate.py
+++
@@ -1,8 +0,0 @@
-import pprint
-file = open("images.txt")
-names = file.readlines()
-for name in names:  
-  print("- src: " + name.strip())
-  print("  title: ")
-  print("  params: ")
-  print("    caption: ")
